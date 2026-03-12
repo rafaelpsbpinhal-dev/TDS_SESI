87f27e02d665a19f245efac36e2e3c84ba72a3b1
rafaelpsbpinhal-dev <rafaelpsbpinhal@gmail.com>
2026-03-12 12:14:23 -0300

    Estruturas de controle

---
+++ controle/else_if.js
@@ -0,0 +1,15 @@
+// Desafio 
+let n1 = 2, n2 = 6; 
+let media;
+
+media = (n1 + n2) /2;
+
+if ( media >= 7.5 ) {
+    console.log ("aprovado");
+}
+else if (media < 7.5 && media >= 5,0) {
+    console.log ("recuperação");
+}
+else {
+    console.log ("reprovado");
+}
---
+++ controle/if.js
@@ -0,0 +1,4 @@
+let idade = 16;
+if (idade >= 18) {
+    console.log ("Você é maior de idade!");
+}
