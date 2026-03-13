b04d3b4d706ca58d0674f811d3da285b1bb95ed7
rafaelpsbpinhal-dev <rafaelpsbpinhal@gmail.com>
2026-03-13 12:16:10 -0300

    array

---
+++ array/array1.js
@@ -0,0 +1,13 @@
+//  Índices         0         1             2           3
+const frutas = ["abacaxi" , "laranja" , "banana" , "melancia"];
+console.log (frutas [0]);
+console.log (frutas [1]);
+console.log (frutas [2]);
+console.log (frutas [3]);
+console.log (frutas);
+console.log (frutas.length); // qauntidade de índices dentro do array
+ 
+frutas[3] = "morango"
+console.log(frutas);
+
+
---
+++ array/array2.js
@@ -0,0 +1,13 @@
+
+const nomes = []
+nomes [0] = "rafael";
+nomes [1] = "motta";
+nomes [2] = "sther";
+nomes [3] = "uncine";
+nomes [4] = "karine";
+console.log (amigos [0]);
+console.log (amigos [1]);
+console.log (amigos [2]);
+console.log (amigos [3]);
+console.log (amigos [4]);
+
