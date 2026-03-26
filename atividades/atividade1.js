4559b7f13dfbd2eb08de9a2cd79e3edae80c7b04
rafaelpsbpinhal-dev <rafaelpsbpinhal@gmail.com>
2026-03-26 11:56:40 -0300

    Atividade 1

---
+++ atividades/atividade1.js
@@ -0,0 +1,12 @@
+// A 
+ 
+const produto = {
+    nome: "tenis",
+    preco: 650,
+    estoque: 1
+}
+    console.log(`o produto ${produto.nome} tem ${produto.estoque} unidade em estoque e ele custa  ${produto.preco} reais`); 
+
+
+
+
